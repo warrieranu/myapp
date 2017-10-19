97c0a132102f3fb9f00d5c220ecf6455d0953757
Anu Warrier <anuwarrier@ymail.com>
2017-10-19 09:42:54 +0000

    facebook

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,25 +10,24 @@ class User < ApplicationRecord
     has_many :course_enrollments
     has_many :courses, through: :course_enrollments
     
-        def create_user_from_omniauth(auth)
-        create(
-            provider: auth['provider'],
-            uid: auth['uid'],
-            name: auth['info']['name'],
-            email: auth['info']['email'],
-            password: SecureRandom.hex
-            )
+def create_user_from_omniauth(auth)
+    create(
+        provider: auth['provider'],
+        uid: auth['uid'],
+        name: auth['info']['name'],
+        email: auth['info']['email'],
+        password: SecureRandom.hex
+        )
         if user.save
             session[:user_id] = @user.id
             redirect_to user_path(@user)
         else
             redirect_to root_url
         end
-            
-    end
-    
-    def sign_in_from_omniauth(auth)
-        find_by(provider: auth['provider'], uid: auth['uid'] || create_user_from_omniauth)
-    end
-    
 end
+    
+def self.sign_in_from_omniauth(auth)
+    find_by(provider: auth['provider'], uid: auth['uid'] || create_user_from_omniauth)
+end
+    
+
